b83b9cc6e69489d97869f7872c93e780eea54a42
johnsondeng <johnson.jy.deng@gmail.com>
2019-11-22 18:22:36 -0800

    starting notes for searching.hpp

--- searching.hpp
+++ searching.hpp
@@ -3,6 +3,17 @@
 template<class data>
 bool binary_search(data search, data[], int setsize){
     int pivot, end, start;
-
+/*
+*
+* General breakdown is that I half the size of the data,
+that half is equal to the pivot,
+I check the value at the pivot and then half the 
+data size again and check again.
+*
+need to also write a guard against a failed search
+condition where number is between end and start and doesn't
+exist.
+*
+*/
     return false;
 }
