e5e3f4945f96b49f3a3cc8243714f58dfecba623
johnsondeng <johnson.jy.deng@gmail.com>
2019-11-22 17:52:24 -0800

    fleshed out template classes

--- sorting.hpp
+++ sorting.hpp
@@ -1,2 +1,32 @@
 #include <iostream>
 using namespace std;
+
+template<class data>
+void swap(data * A, data * B){
+    data temp = *B;
+    *B = *A;
+    *A = temp;
+}
+
+template<class data>
+void selection_sort(data set[], int setsize)
+{
+    int mindex = 0;
+    int smallest = a[0];
+    int smalldex; //track location of smallest
+
+    while(mindex < setsize){
+        int scan = mindex+1;
+        for(; scan < setsize; scan++){
+            if(a[scan] < smallest){
+                smallest = a[scan];
+                smalldex = scan;
+            }//if
+        }//for
+        if(a[smalldex]<a[mindex]){
+            swap(&a[smalldex], &a[mindex]);
+        }//if-swap
+        mindex++;
+        smallest = a[mindex];
+    }
+}
